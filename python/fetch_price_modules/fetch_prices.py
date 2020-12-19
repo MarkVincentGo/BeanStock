90c6934feb3da9250dcbd30035a19cacd0a18e88
Mark Go <mark.vince.go@gmail.com>
2020-12-19 10:29:28 -0800

    change finnhub api to environment var

--- python/fetch_price_modules/fetch_prices.py
+++ python/fetch_price_modules/fetch_prices.py
@@ -32,7 +32,7 @@ def on_open(ws):
 
 if __name__ == "__main__":
     websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("wss://ws.finnhub.io?token=bspbelvrh5r8ktikede0",
+    ws = websocket.WebSocketApp("wss://ws.finnhub.io?token=f{os.environ.get('FINNHUB_API_KEY', '')},
                               on_message = on_message,
                               on_error = on_error,
                               on_close = on_close)
